b0bfe3612ddae07738ef4425d7826faf86ab0d7b
JOguino <meteorowens45@hotmail.com>
2018-04-28 22:49:05 -0400

    Update clock.py

--- clock.py
+++ clock.py
@@ -48,16 +48,6 @@ class Clock(tkinter.Label):
                         self.display_time = self.time
                         self.config(text=self.display_time)
                 self.after(200, self.tick)
-
-
-##        def blink_colon(self):
-##                """ Blink the colon every second """
-##                if ':' in self.display_time:
-##                        self.display_time = self.display_time.replace(':',' ')
-##                else:
-##                        self.display_time = self.display_time.replace(' ',':',1)
-##                self.config(text=self.display_time)
-##                self.after(1000, self.blink_colon)
                 
 
 class Room_Temperature_Frame(tkinter.Label):
