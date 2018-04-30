facedb63ea095b77d5ad550e67587c8d5c0b00fb
JOguino <meteorowens45@hotmail.com>
2018-04-29 21:16:40 -0400

    Update Clock.py

--- Clock.py
+++ Clock.py
@@ -1,17 +1,6 @@
 #!/usr/bin/python3
-
 import tkinter
 import time
-from sense_hat import SenseHat
-import OWMInterface as OWM
-import PixelArray as PA
-import frames
-
-
-sense = SenseHat()
-sense.clear()
-sensorData = {'humidity': sense.get_humidity(), 'temperature': sense.get_temperature(),'pressure': sense.get_pressure()}
-
 
 class Clock(tkinter.Label):
         """ Class that contains the clock widget and clock refresh """
@@ -45,72 +34,4 @@ class Clock(tkinter.Label):
                         self.display_time = self.time
                         self.config(text=self.display_time)
                 self.after(200, self.tick)
-                
-
-if __name__ == "__main__":
-        """
-        Create a tkinter window and populate it with elements
-        One of those elements merely happens to include the clocks.
-
-        The clock widget can be configure()d like any other Label widget.
-        Nothing special needs to be added to main(). The Clock class
-            updates the widget automatically when you create the widget.
-
-        """
-
-        # Create window and frame
-        window = tkinter.Tk()
-        window.configure(background = 'white')
-##        window.attributes('-fullscreen', True)
-        mainframe = tkinter.Frame(width=800, height=600, background="#292b5d")
-        mainframe.pack(fill="both", expand=True, padx=20, pady=20)
-
-        clock_frame  = tkinter.Frame(window, width=800, height=600 )
-        clock_frame.place(in_=mainframe, anchor="c", relx=.5, rely=.5)
-        
-        # Add the frame elements, including the clock like any other element
-        #        top blank line
-        #        time
-        #        bottom blank line
-        top_blank_label = tkinter.Label(clock_frame, text=" ").pack() 
-        clock1 = Clock(clock_frame)
-        clock1.pack()
-        clock1.configure(bg='#292b5d',fg='silver',font=("alfie",200))
-        bottom_blank_label = tkinter.Label(clock_frame, text=" ").pack()
-
-        
-
- #       tkinter.Label(clock_frame, text="Have a nice day.",fg='white',bg='#292b5d').pack()
-        
-       
-##       outside temperature 
-        outside_temperature = frames.Outside_Temperature(window)
-        outside_temperature.place(in_=mainframe, anchor="c", relx=1/6.0, rely=.2)
-        
-        
-##        outside_pressure
-        outside_pressure = frames.Outside_Pressure(window)
-        outside_pressure.place(in_=mainframe, anchor="c", relx=3/6.0, rely=.2)
-        
-##        outside humidity
-        outside_humidity = frames.Outside_Humidity(window)
-        outside_humidity.place(in_=mainframe, anchor="c", relx=5/6.0, rely=.2)
-
-##        Create Room Values
-##        room temperature
-        room_temperature = frames.Room_Temperature(window)
-        room_temperature.place(in_=mainframe, anchor="c", relx=1/6.0, rely=.8)
-
-##        room pressure
-        room_pressure = frames.Room_Pressure(window)
-        room_pressure.place(in_=mainframe, anchor="c", relx=3/6.0, rely=.8)
-
-##        room humidity
-        room_humidity = frames.Room_Humidity(window)
-        room_humidity.place(in_=mainframe, anchor="c", relx=5/6.0, rely=.8)
-
-
-        window.mainloop()
-        
-
 
