7a3c0b458587b0fe395849050197609d0e70fa73
Nazli Goller <ngoller3@gatech.edu>
2018-04-30 13:28:27 -0400

    Icons

--- main_off_board_mac.py
+++ main_off_board_mac.py
@@ -2,11 +2,13 @@
 
 import tkinter
 import time
+import os
 #from sense_hat import SenseHat
 import OWMInterface as OWM
 import PixelArray as PA
-import Frames as frames
+import Frames_off_board_mac as frames
 import Clock as clock
+from PIL import Image, ImageTk
 
 
 if __name__ == "__main__":
@@ -24,10 +26,10 @@ if __name__ == "__main__":
         window = tkinter.Tk()
         window.configure(background = 'white')
 ##        window.attributes('-fullscreen', True)
-        mainframe = tkinter.Frame(width=800, height=600, background="#292b5d")
+        mainframe = tkinter.Frame(width=1280, height=800, background="#292b5d")
         mainframe.pack(fill="both", expand=True, padx=20, pady=20)
 
-        clock_frame  = tkinter.Frame(window, width=800, height=600 )
+        clock_frame  = tkinter.Frame(window, width=1280, height=800 )
         clock_frame.place(in_=mainframe, anchor="c", relx=.5, rely=.5)
         
         # Add the frame elements, including the clock like any other element
@@ -47,7 +49,7 @@ if __name__ == "__main__":
        
 ##       outside temperature 
         outside_temperature = frames.Outside_Temperature(window)
-        outside_temperature.place(in_=mainframe, anchor="c", relx=1/6.0, rely=.2)
+        outside_temperature.place(in_=mainframe, anchor="w", relx=.45/6.0, rely=.2)
         
         
 ##        outside_pressure
@@ -77,20 +79,29 @@ if __name__ == "__main__":
         chance_rain_label = tkinter.Label(chance_rain , text="30%", fg='white',bg='#292b5d',font=("alfie",60)).pack()
         
 ##        weather condition
-        weather_string = ["clear sky",
-                          "few clouds",
-                          "scattered clouds" ,
-                          "broken clouds",
-                          "shower rain",
-                          "rain",
-                          "thunderstorm",
-                          "snow",
-                          "mist"]
-##        weather_string = "scattered clouds"
+        weather_stuff = [("clear sky", "01d.png"),
+                          ("few clouds", "02d.png"),
+                          ("scattered clouds", "03d.png"),
+                          ("broken clouds", "04d.png"),
+                          ("shower rain", "09d.png"),
+                          ("rain", "10d.png"),
+                          ("thunderstorm", "11d.png"),
+                          ("snow", "13d.png"),
+                          ("mist", "50d.png")]
+        
+        current_weather = 5
+        
         weather = tkinter.Frame(width=200, height=50)
         weather.place(in_=mainframe, anchor="w", relx=.45/6.0, rely=.1)
-        weather_label = tkinter.Label(weather , text= weather_string[2], fg='white',bg='#292b5d',font=("alfie",60)).pack()
+        weather_label = tkinter.Label(weather , text= weather_stuff[current_weather][0], fg='white',bg='#292b5d',font=("alfie",60)).pack()
         
+        load = Image.open(os.path.join("icons/", weather_stuff[current_weather][1]))
+        render = ImageTk.PhotoImage(load)
+        
+        weather_logo = tkinter.Frame(width=200, height=50, background="#292b5d")
+        weather_logo.place(in_=mainframe, anchor="c", relx=3/6.0, rely=.1)
+        weather_logo_label = tkinter.Label(weather_logo, background="#292b5d", image=render,font=("alfie",60)).pack()
+
 
 
 
